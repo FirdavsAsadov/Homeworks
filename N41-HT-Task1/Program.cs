2dfba31ad9cb8aabddff51ecda12d21d19b39d6c
Firdavs Asadov <toshmurodovj13@gmail.com>
2023-09-23 16:09:52 +0500

    Uyga Vazifalar tayyor

---
+++ N41-HT-Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using N41_HT_Task1;
+
+var queue = new QUEUE<int>();
+    
+var tasks = new List<Task>()
+{
+    new(() => queue.Enqueue(5)),
+    new(() => queue.Enqueue(21)),
+    new(() => queue.Enqueue(64)),
+    new(() => queue.Enqueue(21)),
+};
+    
+Parallel.ForEach(tasks, task => task.Start());
+await Task.WhenAll(tasks);
+    
+Console.WriteLine(queue.Dequeue());
+Console.WriteLine(queue.Dequeue()); 
+
+namespace N41_HT_Task1
+{
+}
---
+++ N43-Ht-Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿using N43_Ht_Task1.Interfeys;
+using N43_Ht_Task1.Service;
+
+namespace N43_Ht_Task1
+{
+    var user1 = new User("G'ishtmat", "Toshmatov", true);
+
+    var userService = new UserService();
+    userService.Create(user1);
+    var employee = new EmployeeService(userService);
+    var performane = new PerformanceService(userService);
+    var accountService = new AccountService(performane, employee);
+    await accountService.CreateReportAsync(user1.Id);
+}
